b5a9faada4e4e5f0b3e4333618518ceb4949bc57
Damontae Terry <lormobtoottoot@gmail.com>
2026-06-05 08:30:44 -0400

    [BLOCK 021] Add listing ProofPack bridge

---
+++ src/lib/listingProofPackBridge.js
@@ -0,0 +1,23 @@
+﻿import { createProofPack } from "./proofPackContract.js";
+import { recordSellCoreAction } from "./sellcoreActionControlCenter.js";
+
+export function attachProofPackToListing(listing = {}) {
+  const proofPack = createProofPack(listing);
+
+  const enhancedListing = {
+    ...listing,
+    proofPack
+  };
+
+  recordSellCoreAction("proofpack_attached", {
+    listingId: listing.id || null,
+    proofPackVersion: proofPack.proofPackVersion,
+    verificationStatus: proofPack.verificationStatus
+  });
+
+  return enhancedListing;
+}
+
+export function hasProofPack(listing = {}) {
+  return Boolean(listing.proofPack && listing.proofPack.proofPackVersion);
+}

02db1648985d08ecded5f1770267250c843c4a85
completelyunanimous-jpg <completelyunanimous@gmail.com>
2026-09-04 22:45:20 -0400

    Bump Mobile Builder updater for Observer UI

--- public/3d-mobile-duplicate/sw.js
+++ public/3d-mobile-duplicate/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const VERSION='mobile-builder-v6';
+const VERSION='mobile-builder-v7';
 const CACHE_PREFIX='mobile-builder-';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(VERSION).then(c=>c.addAll(SHELL)).catch(()=>{}).then(()=>self.skipWaiting())));
